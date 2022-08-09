253df8657df55d719681f0761995eaeaf2750edb
William <williamsegree@gmail.com>
2022-08-09 14:02:40 -0700

    Apartment Resource completed

---
+++ spec/models/apartment_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+
+
+RSpec.describe Apartment, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has to be real' do
+    expect{ Apartment.new }.to_not raise_error
+  end
+end
